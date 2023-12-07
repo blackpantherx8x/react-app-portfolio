225f012a2679d8a06bfe61f3aee8bb30246c15b5
blackpantherx8x <weiss.alexander1988@gmail.com>
2023-12-07 20:40:13 +0100

    Add Email to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       <form action="" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-500 text-gray-300'>Contact</p>
-          <p className='text-gray-300 py-4'>Submit the form below or shoot me an email myemail@gmail.com</p>
+          <p className='text-gray-300 py-4'>Submit the form below or shoot me an email at - aweiss.webdev@gmail.com</p>
         </div>
         <input className='p-2 bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Your Email' name='email' />
